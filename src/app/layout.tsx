9e77c74a2ca2ed82a2037a5fbda7439604cba037
notcharliee <saunderscharlie@hotmail.com>
2024-02-14 21:31:55 +0000

    feat: add og-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,31 @@ import { type Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "Linktree Clone",
-  description:
-    "Hello there! I made this site to learn how to work with MySQL, Drizzle ORM, and Planetscale DB.",
+  description: "Hello there! I made this site to learn how to work with MySQL, Drizzle ORM, and Planetscale DB.",
+  metadataBase: new URL("https://linktree.charliee.dev"),
+  openGraph: {
+    type: "website",
+    locale: "en_GB",
+    url: "https://linktree.charliee.dev",
+    title: "Linktree Clone",
+    description: "Hello there! I made this site to learn how to work with MySQL, Drizzle ORM, and Planetscale DB.",
+    siteName: "Linktree Clone",
+    images: [
+      {
+        url: "https://linktree.charliee.dev/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Linktree Clone",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Linktree Clone",
+    description: "Hello there! I made this site to learn how to work with MySQL, Drizzle ORM, and Planetscale DB.",
+    images: ["https://linktree.charliee.dev/og-image.png"],
+    creator: "@notchxrliee",
+  },
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
 
